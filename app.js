cb20ac5113cc04e34351169df51c6a2a9854da7f
Brian Besaw <bjbesaw@outlook.com>
2017-08-29 14:21:49 -0400

    studying randomization anomaly

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let STORE = {
   quizQs: [],             // question bank
   appState: 'front',      // 'front', 'quiz', 'end'
   quizQ: 1,               // current question in current quiz
-  quizNumQs: 20,           // total questions in current quiz
+  quizNumQs: 5,           // total questions in current quiz
   quizQState: 'ask',      // 'ask', 'feedback-C', 'feedback-W'
   quizScore: null,        // running score for current quiz
   ansArr: [               // answer order for current question
@@ -70,19 +70,26 @@ function quizHandler(){
   });
 }
 
+
+
 function randAns(){
   console.log(STORE.ansArr);
   // STORE.ansArr[0] = {aID: 'cAns', aRand: Math.random()};
   // STORE.ansArr[1] = {aID: 'wAns1', aRand: Math.random()};
   // STORE.ansArr[2] = {aID: 'wAns2', aRand: Math.random()};
   // STORE.ansArr[3] = {aID: 'wAns3', aRand: Math.random()};
-  STORE.ansArr.forEach(function(ans){
+  // STORE.ansArr.forEach(function(ans){
+  //   ans.aRand = Math.random();
+  // });
+  // STORE.ansArr.sort((a,b) => a.aRand - b.aRand);
+  const newArr = STORE.ansArr.map(function(ans){
     ans.aRand = Math.random();
-  });
-  STORE.ansArr.sort((a,b) => a.aRand - b.aRand);
-  
+    return ans;
+  }).sort((a,b) => a.aRand - b.aRand); 
+  STORE.ansArr = newArr;
   console.log(STORE.ansArr);
 }
+
 
 function randQs(){
   
