678e5658b5e78e8153c114dd000fee31319aab83
Brian Besaw <bjbesaw@outlook.com>
2017-08-28 23:26:32 -0400

    added css and more questions

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
 'use strict';
 let STORE = {
-  quizQs: [
-    {qID: 1,
-      qTxt: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
-      cAns: 'Wham',
-      wAns1: 'Simon and Garfunkel',
-      wAns2: 'Right Said Fred',
-      wAns3: 'Duran Duran'},
-    {qID: 2,
-      qTxt: 'Speed skating originated in which country?',
-      cAns: 'Netherlands',
-      wAns1: 'Russia',
-      wAns2: 'Canada',
-      wAns3: 'Norway'},
-    {qID: 3,
-      qTxt: 'In the book The Last Of The Mohicans what was the name of Chingachgook\'s only son?',
-      cAns: 'Uncas',
-      wAns1: 'Mingo',
-      wAns2: 'Lightfoot',
-      wAns3: 'Magua'},
-    {qID: 4,
-      qTxt: 'In which continent did the ostrich originate?',
-      cAns: 'Africa',
-      wAns1: 'Australia',
-      wAns2: 'North America',
-      wAns3: 'Asia'},
-    {qID: 5,
-      qTxt: 'Julius Caesar said "The die is cast" after crossing which river?',
-      cAns: 'Rubicon',
-      wAns1: 'Danube',
-      wAns2: 'Thames',
-      wAns3: 'Tiber'},
-  ],
+  quizQs: [],
   appState: 'front',     // 'front', 'quiz', 'end'
   quizQ: 1,
+  quizNumQs: 3,
   quizQState: 'ask',      // 'ask', 'feedback-C', 'feedback-W'
   quizScore: null,
   ansArr: [
@@ -53,6 +23,8 @@ function frontHandler(){
   $('#welcome-page').on('click', '#welcome-submit', event => {
     console.log('front handler ran');
     STORE.appState = 'quiz';
+    initialize();
+    randAns();
     renderAppQuiz();
   });
 }
@@ -65,41 +37,54 @@ function quizHandler(){
     
     if(STORE.quizQState === 'ask'){
       let inputValue = $('input[name=answer]:checked').val();
-      console.log(inputValue);
-      console.log(STORE.quizQs[0].cAns);
+      STORE.userAns = inputValue;
       if (inputValue === 'cAns'){
         STORE.quizQState = 'feedback-C';
         STORE.quizScore += 1; 
       } else {
         STORE.quizQState = 'feedback-W';
-        STORE.userAns = inputValue;
       }
     }
     else{
       STORE.quizQ += 1;
       STORE.quizQState = 'ask';
+      randAns();
     }
-  if (STORE.quizQ > STORE.quizQs.length){
-    STORE.appState = 'end';
-  }
+
+    if (STORE.quizQ > STORE.quizNumQs){
+      STORE.appState = 'end';
+    }
+    
     renderAppQuiz();
+
   });
+}
+
+function randAns(){
+  STORE.ansArr[0] = {aID: 'cAns', aRand: Math.random()};
+  STORE.ansArr[1] = {aID: 'wAns1', aRand: Math.random()};
+  STORE.ansArr[2] = {aID: 'wAns2', aRand: Math.random()};
+  STORE.ansArr[3] = {aID: 'wAns3', aRand: Math.random()};
+  STORE.ansArr.sort((a,b) => a.aRand - b.aRand);
 }
 
 // End Handler
 function endHandler(){
   $('#end-of-quiz').on('click', '#end-submit', event => {
     console.log('end handler ran');
-    STORE.appState = 'front';
+    STORE.appState = 'quiz';
+    initialize();
     renderAppQuiz();
   });
 }
 
-
-
-
+function initialize(){
+  STORE.quizQ = 1;
+  STORE.quizScore = 0;
+}
 
 function handleQuizApp() {
+  loadQs();
   renderAppQuiz();
   frontHandler();
   quizHandler();
--- render.js
+++ render.js
@@ -3,18 +3,36 @@
 function renderAppQuiz(){
   console.log('render ran');
 
-  
-  // randomize answer array
-  if(STORE.appState === 'quiz' && STORE.quizQState === 'ask'){
-    STORE.ansArr[0] = {aID: 'cAns', aRand: Math.random()};
-    STORE.ansArr[1] = {aID: 'wAns1', aRand: Math.random()};
-    STORE.ansArr[2] = {aID: 'wAns2', aRand: Math.random()};
-    STORE.ansArr[3] = {aID: 'wAns3', aRand: Math.random()};
-    STORE.ansArr.sort((a,b) => a.aRand - b.aRand);
+  adjPanels();
+
+  if(STORE.appState === 'quiz'){
+
+    $('#question').html(getQATempStr());
+
+    if(STORE.quizQState !== 'ask'){
+      if(STORE.quizQState === 'feedback-W'){
+        $('[for='+STORE.userAns+']').addClass('wrong-answer');
+        $('#question-result').addClass('wrong-answer');
+        $('#question-result').text('That is incorrect');
+      }
+      else if(STORE.quizQState === 'feedback-C'){
+        $('#question-result').addClass('correct-answer');
+        $('#question-result').text('Correct!!');
+      }
+      $('[for=cAns]').addClass('correct-answer');
+      $('#answer input[type=radio]').hide();
+      $('#quiz-submit').text('Next');
+    }
   }
-  
+
+  else if(STORE.appState === 'end'){
+    $('#end-of-quiz').html(getEndStr());
+  }
+}
+
+function adjPanels(){
   $('.js-section').addClass('hidden');
-  // 'front' 'quiz' 'end'
+  
   switch(STORE.appState){
     case 'front':
       $('#welcome-page').removeClass('hidden');
@@ -26,19 +44,12 @@ function renderAppQuiz(){
       $('#end-of-quiz').removeClass('hidden');
       break;
   }
-
-  $('#question').html(getQATempStr());
-
-  if(STORE.quizQState === 'feedback-W' || STORE.quizQState === 'feedback-C'){
-    $('[for='+STORE.userAns+']').addClass('wrong-answer');
-    $('[for=cAns]').addClass('correct-answer');
-    $('#answer input[type=radio]').hide();
-    $('#quiz-submit').text('Next');
-  }
 }
 
 function getQATempStr(){
   return `
+  <h2 id="question-result"></h2>
+  <h2>Question ${STORE.quizQ}</h2>
   <p id='q-text'>${STORE.quizQs[STORE.quizQ-1].qTxt}</p>
   <div id='answer'>
     <input type='radio' id='${STORE.ansArr[0].aID}' name='answer' value='${STORE.ansArr[0].aID}'>
@@ -53,8 +64,14 @@ function getQATempStr(){
   <div>
     <button id='quiz-submit' class='js-button' type='submit'>Submit</button>
   </div>
+  <div class="footer">Current Score:   <span class="score">${STORE.quizScore}</span></div>
   `;
-
 }
 
-
+function getEndStr(){
+  return `
+    <h2>End of Quiz</h2>
+    <p>your score is <span class="score">${STORE.quizScore}</span> out of <span class="score">${STORE.quizNumQs}</span></p>
+    <button id='end-submit' class='js-button' type='button'>Quiz Again</button>
+  `
+}
